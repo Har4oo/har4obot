fd81087dbe478854d86cbb6a8031aa620f3144dd
Har4oo <harihharalampiev@gmail.com>
2023-11-04 00:02:34 +0200

    Token insertion

--- src/main/java/com/example/har4o_bot/Har4oBotApplication.java
+++ src/main/java/com/example/har4o_bot/Har4oBotApplication.java
@@ -1,5 +1,8 @@
 package com.example.har4o_bot;
 
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.utils.cache.CacheFlag;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +10,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Har4oBotApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Har4oBotApplication.class, args);
+		JDABuilder builder = JDABuilder.createDefault("MTE3MDExNDAyODA5NDM3Mzk1OQ.GsFFU8.yUaZUyJWcOXQ9TF4a_zvi6t0Em5_pbTnpr01YI");
+		
+		builder.build();
 	}
 
 }
