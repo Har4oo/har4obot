37b424bb4e5f8ecbea0f6e47a2375c37bbff8a12
Har4oo <harihharalampiev@gmail.com>
2023-11-19 20:57:19 +0200

    Dotenv libraly include

--- src/main/java/com/example/har4o_bot/Bot.java
+++ src/main/java/com/example/har4o_bot/Bot.java
@@ -1,24 +1,20 @@
 package com.example.har4o_bot;
 
-import com.example.har4o_bot.events.TestEvent;
+import com.example.har4o_bot.events.PavkataEventListener;
 import io.github.cdimascio.dotenv.Dotenv;
-import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.sharding.DefaultShardManager;
-import net.dv8tion.jda.api.sharding.DefaultShardManagerBuilder;
-import net.dv8tion.jda.api.sharding.ShardManager;
 
-import javax.security.auth.login.LoginException;
 import java.util.List;
 
 public class Bot {
     public static void main(String[] args) {
-        JDABuilder.createLight()
-                .addEventListeners(new TestEvent())
+        Dotenv config = Dotenv.configure().load();
+        String token = config.get("TOKEN");
+        JDABuilder.createLight(token)
+                .addEventListeners(new PavkataEventListener())
                 .enableIntents(List.of(GatewayIntent.GUILD_MESSAGES, GatewayIntent.MESSAGE_CONTENT))
                 .setStatus(OnlineStatus.ONLINE)
                 .setActivity(Activity.watching("YouTube Kids"))
